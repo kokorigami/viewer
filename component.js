a7d944db27c8d84df1389c25d90afab517582a68
Cindy Zhang <cixzhang@gmail.com>
2016-02-11 17:39:58 -0800

    fixing string element attributes in component.js

--- component.js
+++ component.js
@@ -1,13 +1,17 @@
-function Component(name, parent, _proto, extend) {
+function Component(name, _proto, parent, extend) {
+  parent = parent || HTMLElement;
+
   var proto = {};
   var protoKeys = Object.keys(_proto || {});
 
   protoKeys.forEach(function (key) {
     var attr = _proto[key];
-    if (typeof attr === 'string') {
-      attr = createElementAttribute(attr);
-    } else if (typeof attr === 'function') {
+    if (typeof attr === 'function') {
       attr = createFunctionAttribute(attr);
+    } else if (typeof attr === 'string') {
+      attr = createStringElementAttribute(attr);
+    } else if (!isDefinedProperty(attr)) {
+      attr = createElementAttribute(attr);
     }
 
     proto[key] = attr;
@@ -19,10 +23,23 @@ function Component(name, parent, _proto, extend) {
 
 module.exports = Component;
 
+function isDefinedProperty(prop) {
+  var isDataProp = 'value' in prop || 'writable' in prop;
+  var isAccessProp = 'get' in prop || 'set' in prop;
+  return isDataProp || isAccessProp;
+}
+
 function createElementAttribute(name) {
   return {
     get: function() { return JSON.parse(this.getAttribute(name)); },
     set: function(val) { return this.setAttribute(name, JSON.stringify(val)); }
+  };
+}
+
+function createStringElementAttribute(name) {
+  return {
+    get: function() { return this.getAttribute(name); },
+    set: function(val) { return this.setAttribute(name, val); }
   };
 }
 
