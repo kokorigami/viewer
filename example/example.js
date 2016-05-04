3e452465a232992035efbd6903fc2339d87d38bf
Cindy Zhang <cixzhang@gmail.com>
2016-05-03 21:27:25 -0700

    adding frame navigation

--- example/example.js
+++ example/example.js
@@ -3,4 +3,14 @@ var heartModel = require('./heart.json');
 
 var viewer = new KokorigamiViewer();
 document.body.appendChild(viewer.el);
-viewer.data = heartModel;
+viewer.model = heartModel;
+viewer.play();
+
+viewer.stream.onValue(function (val) {
+  console.log(val);
+});
+
+var prev = document.getElementById('prev');
+var next = document.getElementById('next');
+prev.addEventListener('click', viewer.prev);
+next.addEventListener('click', viewer.next);
--- src/viewer.js
+++ src/viewer.js
@@ -1,34 +1,69 @@
 var Renderer = require('./renderer.js');
 var Model = require('./model.js');
 var viewerHTML = require('./viewer.html');
+var Kefir = require('kefir');
 
 var Viewer = function (el) {
   this.el = el || document.createElement('div');
   this.el.innerHTML = viewerHTML;
 
   var canvas = this.el.querySelector('canvas');
+  this._ = {
+    model: new Model,
+    progress: 0,
+    renderer: new Renderer(canvas),
+    emitter: null
+  };
 
-  this.model = new Model();
-  this.renderer = new Renderer(canvas);
+  this.stream = Kefir.stream(function (emitter) {
+    this._.emitter = emitter;
+    return;
+  }.bind(this));
+
   return this;
 };
 
 Viewer.prototype = {};
 
-Object.defineProperty(Viewer.prototype, 'data', {
+Object.defineProperty(Viewer.prototype, 'model', {
   enumerable: true,
   set: function (data) {
-    var lastFrame = data.frames.length - 1;
-
-    this.model.set(data);
-    this.renderer.data(this.model.frameGeometry(lastFrame));
-    this.renderer.play();
+    this._.model.set(data);
+    this.progress = 0; // reset progress
     return this.model;
   },
 
   get: function () {
-    return this.model;
+    return this._.model;
   }
 });
+
+Object.defineProperty(Viewer.prototype, 'progress', {
+  enumerable: true,
+  set: function (progress) {
+    progress = Math.max(progress, 0);
+    progress = Math.min(progress, this.model.frames.length);
+    this._.progress = progress;
+    this._.renderer.data(this.model.frameGeometry(progress));
+    this._.emitter.emit(progress);
+    return this.progress;
+  },
+
+  get: function () {
+    return this._.progress;
+  }
+});
+
+Viewer.prototype.play = function () {
+  this._.renderer.play();
+};
+
+Viewer.prototype.next = function () {
+  this.progress++;
+};
+
+Viewer.prototype.prev = function () {
+  this.progress--;
+};
 
 module.exports = Viewer;
