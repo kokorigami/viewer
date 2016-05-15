b3bc5aafb0945d835eb1dc39c72fd1203e9b5c69
Cindy Zhang <cixzhang@gmail.com>
2016-05-14 18:56:02 -0700

    functioning? reporting

--- runner.js
+++ runner.js
@@ -1,54 +1,81 @@
 
+var flags = require('flags');
+flags.defineInteger('timeout', 0, 'Connection timeout to start tests.');
+flags.defineBoolean('keep', false, 'Keep the test server up after tests are run.');
+flags.defineString('reporter', 'spec', 'Test reporter.');
+flags.parse();
+
+var mocha = require('mocha');
 var express = require('express');
 var bodyParser = require('body-parser');
+var events = require('events');
 var app = express();
+var started = false;
+var Reporter = mocha.reporters[flags.get('reporter')];
+var reporter, runner;
 
-var flag = require('flags');
-flag.defineInteger('timeout', 0, 'Timeout to start tests.');
-flag.defineBoolean('keep', false, 'Keep the test server up after tests are run.');
-// TODO: actually keep the server running
-flag.parse();
+if (flags.get('timeout') > 0) {
+  setTimeout(function () {
+    if (!started) finish('Connection timed out.', 1);
+  }, flags.get('timeout'));
+}
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-
-app.post('/results', function (req, res) {
-  res.sendStatus(202);
-  var result = req.body;
-  if (result.failures) {
-    print(result.failures + ' tests failed.\n', 1);
-    process.exit(1);
-  }
-  else {
-    print('All tests passed!\n');
-    process.exit(0);
-  }
-});
 
 app.get('/', function (req, res) {
   started = true;
   res.sendFile(__dirname + '/runner.html');
 });
 
-app.listen(3000, function () {
-  print('Test runner server at http://localhost:3000\n');
+app.post('/start', function (req, res) {
+  res.sendStatus(202);
+  runner = new events.EventEmitter(); // mock runner
+  reporter = new Reporter(runner);
+  runner.emit('start');
 });
 
-var started = false;
-if (flag.get('timeout') > 0) {
-  setTimeout(function () {
-    if (!started) {
-      print('Test runner did not start within timeout.', 1);
-      process.exit(1);
-    }
-  }, flag.get('timeout'));
-}
+app.post('/progress', function (req, res) {
+  res.sendStatus(202);
+  var event = req.body.event;
+  var data = req.body.data;
+  var err = req.body.err;
+
+  // mock test functions
+  if (data && data.slow) {
+    var slow = data.slow;
+    data.slow = function () { return slow; };
+  }
+  if (data && data.fullTitle) {
+    var fullTitle = data.fullTitle;
+    data.fullTitle = function () { return fullTitle; };
+  }
+
+  reporter.stats = req.body.stats;
+  runner.emit(event, data, err);
+});
+
+app.post('/end', function (req, res) {
+  res.sendStatus(202);
+  reporter.stats = req.body;
+  runner.emit('end');
+  finish(null, reporter.stats.failures > 0 ? 1 : 0);
+});
+
+app.listen(3000, function () {
+  print('Testing at http://localhost:3000');
+});
 
 function print(message, error) {
   var output = error ? process.stderr : process.stdout;
-  output.write(message);
+  output.write(message + '\n');
+}
+
+function finish(message, code) {
+  if (message) print(message, code);
+  if (!flags.get('keep')) process.exit(code);
 }
 
 process.on('exit', function (code) {
-  print('Exited: ' + code + '\n');
+  print('Exited with code ' + code + '\n');
 });
