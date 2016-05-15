251323c5fc14439dbd7307e92caf7c186261126a
Cindy Zhang <cixzhang@gmail.com>
2016-05-14 20:39:23 -0700

    using karma

---
+++ karma.conf.js
@@ -0,0 +1,24 @@
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha'],
+    files: ['test.bundle.js'],
+    exclude: [],
+    preprocessors: {},
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Firefox'],
+    singleRun: true,
+    concurrency: Infinity,
+    client: {
+      mocha: {
+        reporter: 'html',
+        ui: 'bdd'
+      }
+    }
+  });
+};
