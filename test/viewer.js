171c6130da4a5717bb8527feda0547a146065d0a
Cindy Zhang <cixzhang@gmail.com>
2016-05-15 14:39:45 -0700

    unskipping test

--- test/viewer.js
+++ test/viewer.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect;
 var Viewer = require('../src/viewer.js');
 
-xdescribe('viewer', function () {
+describe('viewer', function () {
   it('can be created', function () {
     var viewer = new Viewer();
     expect(viewer).to.exist;
