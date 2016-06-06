f14f0ec6f33e6c182109670325d157a391b7276d
Cindy Zhang <cixzhang@gmail.com>
2016-06-06 08:33:46 -0700

    adding emitter

--- example/example.js
+++ example/example.js
@@ -3,13 +3,19 @@ var heartModel = require('./heart.json');
 
 var viewer = new KokorigamiViewer();
 document.body.appendChild(viewer.el);
-viewer.model = heartModel;
-viewer.render();
 
 var reset = document.getElementById('reset');
 var prev = document.getElementById('prev');
 var next = document.getElementById('next');
+var frame = document.getElementById('frame');
 
 prev.addEventListener('click', function () { viewer.prev(); });
 next.addEventListener('click', function () { viewer.next(); });
 reset.addEventListener('click', function () { viewer.frame = 0; });
+
+viewer.emitter.on('update', function (newFrame) {
+  frame.value = newFrame;
+});
+
+viewer.model = heartModel;
+viewer.render();
